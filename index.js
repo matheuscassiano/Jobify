24a71ceacff534c89fc65e76425f331de748e722
Matheus Cassiano <matheuscassiano9@gmail.com>
2019-04-07 12:25:20 -0300

    Blocking admin for localhost

--- index.js
+++ index.js
@@ -9,8 +9,17 @@ const dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), { Prom
 
 const port = process.env.PORT || 3000
 
+app.use('/admin', (req, res, next) => {
+    if(req.hostname === 'localhost'){
+        next()
+    }else{
+        res.send('Not allowed')
+    }
+})
+
+app.set('views', path.join(__dirname,'views'))
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname,'public')))
 app.use(bodyParser.urlencoded({
     extended:true
 }))
